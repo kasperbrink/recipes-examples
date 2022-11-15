e3b7bf29a02ff15f7d6bbaf39d20e6379877e9e9
kasperbrink <kasperbrinkjensen@gmail.com>
2022-11-15 19:54:16 +0000

    xgboost first run

--- regression/steps/train.py
+++ regression/steps/train.py
@@ -15,6 +15,6 @@ def estimator_fn(estimator_params: Dict[str, Any] = None):
     """
     if estimator_params is None:
         estimator_params = {}
-    from sklearn.linear_model import SGDRegressor
+    from xgboost import XGBRegressor
 
-    return SGDRegressor(random_state=42, **estimator_params)
+    return XGBRegressor(random_state=42, **estimator_params)
